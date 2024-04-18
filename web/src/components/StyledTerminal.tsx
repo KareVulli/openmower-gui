5e4cc711e92ea8abcbf298fea3b74832f47100e5
Marvin Helstein <m_helstein@hotmail.com>
2024-04-18 18:17:22 +0300

    Fix Terminal styling

--- web/src/components/StyledTerminal.tsx
+++ web/src/components/StyledTerminal.tsx
@@ -2,7 +2,12 @@ import styled from "styled-components";
 
 export const StyledTerminal = styled.div`
   div.react-terminal-wrapper {
-    padding-top: 35px;
+    padding: 0px;
+    font-size: 14px;
+  }
+
+  div.react-terminal-line:before {
+    content: none;
   }
 
   div.react-terminal-wrapper > div.react-terminal-window-buttons {
